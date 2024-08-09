32da65a0adfe404759d628b37fd803572ec65b56
Chen Liang <liangchen76@gmail.com>
2024-08-09 14:09:55 -0500

    initial version

---
+++ OAuth2-test-app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/health')
+def health(): 
+    return "OK", 200
+
+@app.route('/callback')
+def callback():
+    auth_code = request.args.get('code')
+    if auth_code:
+        return jsonify({"message": "Authorization code received", "code": auth_code})
+    else:
+        return jsonify({"error": "No authorization code received"}), 400
+
+if __name__ == '__main__':
+    app.run(port=5001, debug=True)
